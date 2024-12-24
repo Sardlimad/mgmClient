150d598330e436fece90c9c08788a43bd559bfe1
David Sardlimad <genesis.david.08@gmail.com>
2024-12-24 16:00:05 -0500

    Add botón regrear desde clientes a home

--- src/Clients.jsx
+++ src/Clients.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Divider, Grid, Typography } from '@mui/material'
 import React from 'react';
 import ClientTable from './Custom/ClientTable';
 import AddIcon from '@mui/icons-material/Add';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
 
 export const Clients = () => {
@@ -24,6 +25,9 @@ export const Clients = () => {
                 <Grid item>
                     <Button variant="contained" color="primary" sx={{ marginRight: 1 }} startIcon={<AddIcon />} href="/client/create">
                         Registrar
+                    </Button>
+                    <Button variant="outlined" color="secondary" startIcon={<KeyboardReturnIcon />} href="/">
+                        Regresar
                     </Button>
                 </Grid>
             </Grid>
